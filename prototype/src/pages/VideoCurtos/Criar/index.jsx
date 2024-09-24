ef965c42f3aae9e1ffd2ce511cb251feb83b1b58
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-09-23 23:07:26 -0300

    algumas alteraçoes na aba de Videos Curtos

--- prototype/src/pages/VideoCurtos/Criar/index.jsx
+++ prototype/src/pages/VideoCurtos/Criar/index.jsx
@@ -1,7 +1,18 @@
+import Nuvem from '../assets/nuvem.svg'
+
 export default function Criar() {
     return (
         <>
-            <p>Criar</p>
+            <div>
+                <div>
+                    <img src={Nuvem} alt="" />
+                    <div>
+                        <h2>Selecione o vídeo a enviar.</h2>
+                        <p>Ou arraste e solte aqui.</p>
+                        <button>Selecionar Vídeo</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- prototype/src/pages/VideoCurtos/Global/index.jsx
+++ prototype/src/pages/VideoCurtos/Global/index.jsx
@@ -1,7 +1,42 @@
+import Perfil from '../assets/perfil.svg'
+import AddPerfil from '../assets/addperfil.svg'
+import Compartilhar from '../assets/compartilhar.svg'
+import Curtir from '../assets/curtir.svg'
+import Mensagem from '../assets/mensagem.svg'
+import Salvar from '../assets/salvar.svg'
+
 export default function Global() {
     return (
         <>
-            <p>Global</p>
+            <div>
+                <div><p>video</p></div>
+                <div>
+                    <div>
+                        <button><img src={Perfil} alt="" /></button>
+                        <button><img src={AddPerfil} alt="" /></button>
+                    </div>
+
+                    <div>
+                        <button><img src={Curtir} alt="" /></button>
+                        <p>203</p>
+                    </div>
+
+                    <div>
+                        <button><img src={Mensagem} alt="" /></button>
+                        <p>30</p>
+                    </div>
+
+                    <div>
+                        <button><img src={Salvar} alt="" /></button>
+                        <p>11</p>
+                    </div>
+
+                    <div>
+                        <button><img src={Compartilhar} alt="" /></button>
+                        <p>7</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
